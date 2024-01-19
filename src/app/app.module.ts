75b8bd3da3f9c17edc777f127ab66d132ac6e041
Nieves <smilenvs@gmail.com>
2024-01-19 12:30:22 +0100

    Intentando que funcione nb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeRoutedComponent } from './components/shared/home-routed/home-routed.component';
 import { MenuUnroutedComponent } from './components/shared/menu-unrouted/menu-unrouted.component';
 //Nebular
-import { NbThemeModule } from '@nebular/theme';
+import { NbThemeModule,NbSidebarModule, NbLayoutModule, NbActionsModule, NbIconModule, NbIconLibraries } from '@nebular/theme';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,12 +15,23 @@ import { NbThemeModule } from '@nebular/theme';
     MenuUnroutedComponent,
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
     NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbLayoutModule,
+    NbActionsModule,
+    NbIconModule,
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private iconLibraries: NbIconLibraries) {
+    // Registra paquetes de iconos aquí
+    this.iconLibraries.registerFontPack('far', { packClass: 'far', iconClassPrefix: 'fa' });
+  }
+}
--- src/app/components/shared/menu-unrouted/menu-unrouted.component.ts
+++ src/app/components/shared/menu-unrouted/menu-unrouted.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-unrouted',
   templateUrl: './menu-unrouted.component.html',
-  styleUrls: ['./menu-unrouted.component.css']
+  styleUrls: ['./menu-unrouted.component.css'],
 })
 export class MenuUnroutedComponent implements OnInit {
 
