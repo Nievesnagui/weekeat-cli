3685f0a7a16a91b3c9e525e4ce66dd02e99ae640
Nieves <smilenvs@gmail.com>
2024-01-28 11:15:13 +0100

    Details de type

--- src/app/components/admin/admin-type-detail-unrouted/admin-type-detail-unrouted.component.ts
+++ src/app/components/admin/admin-type-detail-unrouted/admin-type-detail-unrouted.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { IType } from 'src/app/model/model.interface';
+import { SessionService } from 'src/app/service/session.service';
+import { TypeService } from 'src/app/service/type.service';
 
 @Component({
   selector: 'app-admin-type-detail-unrouted',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminTypeDetailUnroutedComponent implements OnInit {
 
-  constructor() { }
+  @Input() id: number = 1;
+
+  oType: IType = {} as IType;
+  status: HttpErrorResponse | null = null;
+
+  constructor(
+    private oTypeService: TypeService,
+    private oSessionService: SessionService
+  ) { }
 
   ngOnInit() {
+    this.getOne();
+  }
+
+  getOne(): void {
+    this.oTypeService.getOne(this.id).subscribe({
+      next: (data: IType) => {
+        this.oType = data;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.status = error;
+      }
+    })
   }
 
 }
