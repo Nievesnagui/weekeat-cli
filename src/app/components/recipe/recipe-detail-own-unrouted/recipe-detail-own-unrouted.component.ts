6202fca129ac8d3b4ef80864b30e557154cdf727
Nieves <smilenvs@gmail.com>
2024-02-05 18:37:45 +0100

    Comenzando a hacer cosas con el weekly

--- src/app/components/recipe/recipe-detail-own-unrouted/recipe-detail-own-unrouted.component.ts
+++ src/app/components/recipe/recipe-detail-own-unrouted/recipe-detail-own-unrouted.component.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-import { IRecipe } from 'src/app/model/model.interface';
+import { IRecipe, IUser } from 'src/app/model/model.interface';
 import { RecipeService } from 'src/app/service/recipe.service';
+import { SessionService } from 'src/app/service/session.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-recipe-detail-own-unrouted',
@@ -11,13 +13,28 @@ import { RecipeService } from 'src/app/service/recipe.service';
 export class RecipeDetailOwnUnroutedComponent implements OnInit {
 
   @Input() id: number = 1;
+  strUserName: string = "";
+  oSessionUser: IUser | null = null;
+
 
   oRecipe: IRecipe = { id_user: {} } as IRecipe;
   status: HttpErrorResponse | null = null;
   
   constructor(
+    private oUserService: UserService,
+    private oSessionService: SessionService,
     private oRecipeService: RecipeService,
-  ) { }
+  ) { 
+    this.strUserName = oSessionService.getUsername();
+    this.oUserService.getByUsername(this.oSessionService.getUsername()).subscribe({
+      next: (oUser: IUser) => {
+        this.oSessionUser = oUser;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    });
+  }
 
   ngOnInit() {
     this.getOne();
