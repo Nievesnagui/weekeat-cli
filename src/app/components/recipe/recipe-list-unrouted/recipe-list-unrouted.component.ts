70fdc7956eb597ec93c9060327e8db8562ac4ec1
Nieves <smilenvs@gmail.com>
2024-02-08 17:51:31 +0100

    Cambiando colores, añadiendo component columns

--- src/app/components/recipe/recipe-list-unrouted/recipe-list-unrouted.component.ts
+++ src/app/components/recipe/recipe-list-unrouted/recipe-list-unrouted.component.ts
@@ -59,7 +59,7 @@ export class RecipeListUnroutedComponent implements OnInit {
   getRecipeImageUrl(recipeImage: string | null | undefined): string {
     if (recipeImage) {
       // Si hay una imagen en la base de datos, devuelve la URL de esa imagen
-      return `URL_DE_TU_API_PARA_OBTENER_IMAGEN/${recipeImage}`;
+      return `${recipeImage}`;
     } else {
       // Si no hay imagen en la base de datos, devuelve la URL predeterminada
       return 'https://images.unsplash.com/photo-1586511925558-a4c6376fe65f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=60';
