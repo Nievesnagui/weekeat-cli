e48bc641c612efea971ab8f535d5fe7105e7fc5a
Nieves <smilenvs@gmail.com>
2024-02-19 17:57:20 +0100

    Último commit hasta el jueves

--- src/app/components/shared/login-routed/login-routed.component.ts
+++ src/app/components/shared/login-routed/login-routed.component.ts
@@ -64,7 +64,7 @@ export class LoginRoutedComponent implements OnInit {
 
   loginUser() {
     this.loginForm.setValue({
-      username: 'prueba3',
+      username: 'usuario',
       password: 'weekeat'
     })
   }
