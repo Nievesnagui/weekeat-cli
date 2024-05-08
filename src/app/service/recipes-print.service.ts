9f5f9f43092bfb90aa7af32ab7eac0654d0344f7
Nieves <smilenvs@gmail.com>
2024-05-08 19:03:01 +0200

    Imprime lista de la compra bonica

--- src/app/service/recipes-print.service.ts
+++ src/app/service/recipes-print.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { WeeklyService } from './weekly.service';
-import { ScheduleService } from './schedule.service';
-import { ISchedule, ISchedulePrueba, IWeekly } from '../model/model.interface';
+import { ISchedulePrueba } from '../model/model.interface';
 import { ContentService } from './content.service';
 import { forkJoin } from 'rxjs';
 declare let jsPDF: any;
@@ -12,8 +10,6 @@ declare let jsPDF: any;
 export class RecipesPrintService {
 
   constructor(
-    private oWeeklyService: WeeklyService,
-    private oScheduleService: ScheduleService,
     private oContentService: ContentService
   ) { }
 
@@ -21,12 +17,11 @@ export class RecipesPrintService {
 
   printSchedules = (oSchedulesToPrint: ISchedulePrueba[]): void => {
     var doc = new jsPDF();
-    doc.setFont('Arial');
+    doc.setFont('Helvetica');
     doc.setFontSize(12);
     doc = this.header(doc);
-    //doc = this.content(doc, oSchedulesToPrint); 
     forkJoin(
-      oSchedulesToPrint.map(schedule => 
+      oSchedulesToPrint.map(schedule =>
         forkJoin(
           schedule.recipe.content.map(content =>
             this.oContentService.getOneForPrinting(content.id)
@@ -39,99 +34,97 @@ export class RecipesPrintService {
       const allIngredients = ([] as any[]).concat(...ingredientLists);
       const uniqueIngredients = new Set<string>(Array.from(new Set(allIngredients.map(ingredient => ingredient.ingredient.name))));
 
-      this.printIngredients(doc, uniqueIngredients);
-      doc = this.footer(doc);
-  
-      doc.save('weekeat-my-weekly.pdf');
+      this.printIngredients(doc, uniqueIngredients, 40);
+      this.prettyCircles(doc);
+
+
+      doc.save('weekeat-grocery-list.pdf');
     });
   }
+
+  setBaseColor(doc: any) {
+    doc.setDrawColor(40, 83, 24);
+    doc.setTextColor(40, 83, 24);
+  }
+
   private header(doc: any): any {
 
-    const indigoPastel = "#7887AB";
-
+    this.setBaseColor(doc);
     doc.setFontType('bold');
     doc.setFontSize(20);
-    doc.text('WEEKEAT 2024', 70, 25);
-    //
-    doc.setDrawColor(indigoPastel);
-    doc.line(60, 30, 145, 30);
-    //
-    doc.text('My Weekly', 80, 40);
-    doc.setDrawColor(indigoPastel);
-    doc.line(10, 45, 200, 45);
+    doc.text('My Grocery List', 70, 15);
+    doc.line(10, 25, 200, 25);
     doc.setFontType('normal');
 
     return doc;
   }
 
 
-  private printIngredients(doc: any, ingredients: Set<string>): void {
+  private printIngredients(doc: any, ingredients: Set<string>, footerSize: number): void {
     doc.setFontSize(12);
-    let yOffset = 50; // Inicializar la posición Y
+    doc.setTextColor(0, 0, 0);
+    let yOffset = 40; // Inicializar la posición Y
+    let xOffset = 20; // Inicializar la posición
     const lineHeight = 10; // Altura de la línea
     const pageHeight = 297; // Altura de la página (A4)
-  
+
     ingredients.forEach(ingredient => {
-      const requiredHeight = lineHeight; // Altura requerida para este ingrediente
       const remainingHeight = pageHeight - yOffset; // Altura restante en la página actual
-  
+
       // Verificar si queda suficiente espacio en la página actual para imprimir este ingrediente
-      if (remainingHeight < requiredHeight) {
-        doc.addPage(); // Agregar una nueva página
-        yOffset = 50; // Restablecer la posición Y en la nueva página
+      if (remainingHeight < footerSize) {
+        if (xOffset < 80) {
+          xOffset = 80;
+          yOffset = 40;
+        } else {
+          doc.addPage();
+          xOffset = 20;
+          yOffset = 40;
+        }
+      } else {
+        doc = this.header(doc);
+        doc = this.footer(doc);
       }
-  
+
+      doc.circle(xOffset - 3, yOffset - 1.5, 1.5, 'S')
+
       // Imprimir el ingrediente en la posición actual
-      doc.text(20, yOffset, ingredient);
-  
+      doc.text(xOffset, yOffset, ingredient);
+
       // Actualizar la posición Y para el próximo ingrediente
       yOffset += lineHeight;
     });
   }
-  
-  
-  private linea(doc: any, oSchedule: ISchedulePrueba, linea: number): void {
-    doc.setFontSize(12);
-    const lineHeight = 7; // Altura de línea
-    const pageHeight = 297; // Altura de página (A4)
+
+  private prettyCircles(doc: any): void {
+
+    doc.setFillColor(139, 187, 118);
+    doc.circle(180, 50, 10, 'F');
+
+    doc.setFillColor(119, 158, 102);
+    doc.circle(208, 60, 16, 'F');
+
+    doc.setFillColor(31, 73, 16);
+    doc.circle(180, 100, 20, 'F');
+
+    doc.setFillColor(119, 158, 102);
+    doc.circle(210, 160, 45, 'F');
     
-    // Calcular la altura restante en la página actual
-    const remainingHeight = pageHeight - linea;
-    
-    // Calcular la altura necesaria para imprimir esta línea
-    const requiredHeight = lineHeight * 3; // Asumiendo una altura de 3 líneas
-    
-    // Verificar si queda suficiente espacio en la página actual
-    if (remainingHeight < requiredHeight) {
-      doc.addPage(); // Agregar una nueva página
-      doc = this.header(doc); // Agregar el encabezado a la nueva página
-      linea = 50; // Restablecer la posición de la línea en la nueva página
-    }
-    
-    // Imprimir el nombre de la receta y el proceso en la posición actual
-    doc.text(oSchedule.recipe.name, 20, linea);
-    doc.setFontSize(8);
-    doc.text(oSchedule.recipe.process, 210, linea, 'center');
-  
-    // Actualizar la posición de la línea para la próxima iteración
-    linea += requiredHeight;
+    doc.setFillColor(139, 187, 118);
+    doc.circle(200, 230, 30, 'F');
+
   }
-  
 
   private footer(doc: any): any {
 
-    const indigoPastel = "#7887AB";
-
-    doc.setDrawColor(indigoPastel);
-    doc.line(10, 244, 200, 244);
+    this.setBaseColor(doc);
+    doc.line(10, 264, 200, 264);
     //
     doc.setFontSize(12);
     doc.setTextColor(0, 0, 0);
-    doc.text(11, 254, `CIPFP Ausiàs March`);
-    doc.text(11, 264, `Télf: 961205930`);
-    doc.text(65, 264, `Fax: 961205931`);
-    doc.text(11, 274, `C/Ángel Villena, s/n. 46013 Valencia`);
-    doc.text(11, 284, `secretaria@ausiasmarch.net`);
+    doc.text(11, 274, `WeekEat`);
+    doc.text(155, 274, `Don't think, just cook!`);
+    doc.text(75, 284, `The easiest grocery list`);
 
     return doc;
   }
